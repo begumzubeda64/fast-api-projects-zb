bd55df4335fd7c4d4b25bc4cf45f7947271e40d3
zubeda-abbas <zubeda@getvantage.co>
2026-02-02 17:02:42 +0530

    Update production db

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -4,12 +4,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from urllib.parse import quote_plus
 
 ## sqlite
-SQLALCHAMY_DATABASE_URL = "sqlite:///./todosapp.db"
-engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# SQLALCHAMY_DATABASE_URL = "sqlite:///./todosapp.db"
+# engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 ## postgresql
-# db_pass = quote_plus("Zubu@123456789")
-# SQLALCHAMY_DATABASE_URL = f"postgresql://postgres:{db_pass}@localhost/TodoApplicationDatabase"
+db_pass = quote_plus("Zubu@123456789")
+SQLALCHAMY_DATABASE_URL = f"postgresql://todosapp_sbme_user:SqkGj0mUb8dC8GpwH6MB0kxtVp6FEzke@dpg-d608jv7pm1nc73bcarig-a/todosapp_sbme"
+engine = create_engine(SQLALCHAMY_DATABASE_URL)
 
 ## mysql
 # SQLALCHAMY_DATABASE_URL = f"mysql+pymysql://root:{db_pass}@localhost:3306/TodoApplicationDatabase"
